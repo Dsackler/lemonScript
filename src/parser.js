e946a8d7479c1f61adcf8065bac71417314d6741
Khalid <kfg2000@live.com>
2021-02-19 01:05:51 +0300

    ohm changes done

--- src/parser.js
+++ src/parser.js
@@ -1,4 +1,4 @@
-  
+
 // Parser
 //
 // Exports a default function mapping the source code as a string to the AST.
@@ -6,11 +6,11 @@
 import ohm from "ohm-js"
 
 const lemonScriptGrammar = ohm.grammar(String.raw`lemonScript {
-    Program   	          = Import* Statement*                                                            --program      
+    Program   	          = Import* Statement*                                                            --program
     Import 		            = import id from id                                                             --importDec
-    Statement 	          = const? static? Type id "=" BoolOp                                             --varDec
+    Statement 	          = const? static? Type id "=" Exp                                             --varDecInit
                             | const? static? Type id							                                        --varDec
-                            | Var "=" BoolOp                      		                                    --assignExp
+                            | Var "=" Exp                      		                                    --assignExp
                             | SwitchStatement
                             | FunctionCall
                             | FunctionDec
@@ -21,32 +21,33 @@ const lemonScriptGrammar = ohm.grammar(String.raw`lemonScript {
                             | Print
                             | TypeOf
                             | ReturnStatement
-                            | SliceCrement 
+                            | SliceCrement
                             | continue
                             | break
-                            | BoolOp
-    ClassDec					    = classType id (extends id)? ClassBeginToEnd                                    --classDec
+                            | Exp
+    ClassDec					    = classType id (extends id)? ClassBeginToEnd
     ClassBeginToEnd 	    = openBrace Constructor Statement* closeBrace
     Constructor				    = "plant" "(" Parameters ")" BeginToEnd
-    FunctionDec 			    = functionBeginning static? (Type | void | id) id "(" Parameters ")" BeginToEnd --funcDec
-    FunctionCall          = Var "(" Arguments ")"                                                         --funcCall
-    IfStatement  			    = ifBeginning "(" BoolOp ")" BeginToEnd ElseifStatement* ElseStatement?         --ifStatement
-    ElseifStatement  		  = elifBeginning "(" BoolOp ")" BeginToEnd                                       --ifStatement
-    ElseStatement  		    = elseBeginning BeginToEnd                                                      --elseStatement
-    WhileStatement 		    = whileBeginning "(" BoolOp ")" BeginToEnd                                      --whileStatement
-    ForStatement 			    = forBeginning "(" ForArgs ")" BeginToEnd                                       --forStatement
-    ForArgs          		  = "slice" id "=" BoolOp ";" BoolOp ";" SliceCrement
-    SliceCrement   		    = (id"+=" AddOp | id"-=" AddOp )                                                -- binary
-                            | (id"++" | id"--" )							                                            --unary
-    SwitchStatement 		  = switch "("Var")" SwitchBeginToEnd                                             --switchStatement
+    FunctionDec 			    = functionBeginning static? (Type | void | id) id "(" Parameters ")" BeginToEnd
+    FunctionCall          = Var "(" Arguments ")"
+    IfStatement  			    = ifBeginning "(" Exp ")" BeginToEnd ElseifStatement* ElseStatement?
+    ElseifStatement  		  = elifBeginning "(" Exp ")" BeginToEnd
+    ElseStatement  		    = elseBeginning BeginToEnd
+    WhileStatement 		    = whileBeginning "(" Exp ")" BeginToEnd
+    ForStatement 			    = forBeginning "(" ForArgs ")" BeginToEnd
+    ForArgs          		  = "slice" id "=" Exp ";" Exp ";" SliceCrement
+    SliceCrement   		    = (id "+=" AddOp | id "-=" AddOp )                                              --binary
+                            | (id"++" | id"--" )							                                            --postfix
+    SwitchStatement 		  = switch "("Var")" SwitchBeginToEnd
     SwitchBeginToEnd 		  = openBrace Lemoncase+ Defaultcase? closeBrace
-    Lemoncase 			      = case BoolOp Statement* 
-    Defaultcase					  = default Statement* 
-    Print								  = print"("BoolOp")"
-    TypeOf						    = typeof "("BoolOp")"
-    ReturnStatement		    = return BoolOp?                                                                --returnStatement
-    BeginToEnd 	          = openBrace Statement+ closeBrace 
-    BoolOp 			          = BoolOp conop Bool                                                             --binary
+    Lemoncase 			      = case Exp Statement*
+    Defaultcase					  = default Statement*
+    Print								  = print"("Exp")"
+    TypeOf						    = typeof "("Exp")"
+    ReturnStatement		    = return Exp?
+    BeginToEnd 	          = openBrace Statement+ closeBrace
+    Exp                   = BoolOp
+    BoolOp 			          = BoolOp logop Bool                                                             --binary
                             | Bool
     Bool      	          = Bool relop AddOp                                                              --binary
                             | AddOp
@@ -56,30 +57,31 @@ const lemonScriptGrammar = ohm.grammar(String.raw`lemonScript {
                             | Exponential
     Exponential           = Factor "^" Exponential                                                        --binary
                             | Factor
-    Factor 		            = FunctionCall                 
-                            | ("-" ) Factor                                                               --unary
+    Factor 		            = FunctionCall
+                            | ("-" ) Factor                                                               --prefix
                             | "(" BoolOp ")"                                                              --templateLit
                             | "[" Arguments "]"					                                                  --arrayLit
                             | "{" DictValues "}"				                                                  --objLit
-                            | numlit              
-                            | stringlit           
+                            | numlit
+                            | stringlit
                             | boollit
                             | Var
     numlit       		      = digit+ ("." digit+)?                                                          --literal
     boollit 		          = "sweet" | "sour"                                                              --boolLit
     stringlit 		        = "\"" char* "\""                                                               --strLit
-    char 				          = "\\n" 
-                            | "\\'"  
+    char 				          = "\\n"
+                            | "\\'"
                             | "\\\""
                             | "\\\\"
                             | "\\u{" hexDigit hexDigit? hexDigit? hexDigit? hexDigit? hexDigit?  "}" 			--hex
                             | ~"\"" ~"\\" any
-    Var					          = Var "." Var		                                                                --memberExp        
-                            | Var "["digit+"]"	                                                          --memberExp
+    Var					          = Property
                             | id
+    Property              = Var "." Var		                                                                --dotMemberExp
+                            | Var "["digit+"]"                                                            --memberExp
     Type       			      = ArrayType | types | DictType
     types                 = ("pulp"|"slice"|"taste"|"dontUseMeForEyeDrops") ~alnum
-    ArrayType			        = Type "[]"                                                                     --arrayDec    
+    ArrayType			        = Type "[]"                                                                     --arrayDec
     DictType			        = "<" Type "," Type ">"                                                         --objDec
     void					        = "noLemon" ~alnum
     functionBeginning     = "When life gives you lemons try" ~alnum
@@ -95,26 +97,25 @@ const lemonScriptGrammar = ohm.grammar(String.raw`lemonScript {
     typeof					      = "species"
     openBrace 			      = "BEGIN JUICING" ~alnum
     closeBrace			      = "END JUICING" ~alnum
-    switch 					      = "Pick" ~alnum                   
+    switch 					      = "Pick" ~alnum
     break					        = "chop" ~alnum
     continue 			        = "nextLemon" ~alnum
     return				        = "you get lemonade and" ~alnum
     default				        = "citrusLimon" ~alnum
     const					        = "lemonStain" ~alnum
     static			  	      = "trunk" ~alnum
-    import			  	      = "receive" ~alnum        
+    import			  	      = "receive" ~alnum
     from				          = "from" ~alnum
     keyword   			      = types | void | print | openBrace | closeBrace | switch | break | case | default | classType | const | forBeginning | continue | boollit | typeof
     id        			      = ~keyword letter (alnum | "_")*
-    Arguments 			      = (BoolOp",")* BoolOp			                                                      --unary
-                            |""
-    Parameters			      = ((Type | id) id",")* (Type | id) id	                                          --params
-                            |""
-    DictValues			      = (BoolOp ":" BoolOp ",")* BoolOp ":" BoolOp                                    --params
-                            |""
+    Arguments 			      = ListOf<Exp, ",">
+    Parameters			      = ListOf<Binding, ",">
+    Binding 			        = (Type | id) id
+    DictValues			      = ListOf<KeyValue, ",">
+    KeyValue		          = Exp ":" Exp
     space    				      += "( *)" (~"(* )" any)* "(* )"  				                                        --longComment
                             | "( *)" (~"\n" any)* ("\n" | end) 		 	                                      --comment
-    conop				          = "&&" | "||"
+    logop				          = "&&" | "||"
     relop   			        = "<=" | "<" | "==" | "!=" | ">=" | ">"
     addop 			          = "+" | "-"
     mulop				          = "*"| "/"| "%"
